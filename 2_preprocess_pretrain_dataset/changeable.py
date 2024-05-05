b512dae417788b381ff9c5bfa4ccb079dcdc1441
nerdpai <mien848red@gmail.com>
2024-05-05 13:41:25 +0200

    restruct changeable

--- 2_preprocess_pretrain_dataset/changeable.py
+++ 2_preprocess_pretrain_dataset/changeable.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
+## gz
 gz_file_pathes: list[Path] = [
     # elsewhere
 ]
 
+## json
 json_file_pathes: list[Path] = [
     # elsewhere
 ]
-
 csv_directory: Path = Path(
     # elsewhere
 )
-
 TARGET_SIZE_OF_CSV_IN_MB: int = 30
--- 3_create_vocab/changeable.py
+++ 3_create_vocab/changeable.py
@@ -1,22 +1,10 @@
 from pathlib import Path
 
-
-cc_mined_dir = Path(
-    # elsewhere
-)
-cache_dir = Path(
-    # elsewhere
-)
-save_tokenizer_dir = Path(
-    # elsewhere
-)
-
-CONTENT_COLUMN: str = "raw_content"
+## tokenizer
 NEW_LINE_TOKEN: str = "<newline>"
 UNK_TOKEN: str = "<unk>"
 BOS_TOKEN: str = "<s>"
 EOS_TOKEN: str = "</s>"
-
 special_tokens: list[str] = [
     BOS_TOKEN,
     "<pad>",
@@ -25,10 +13,25 @@ special_tokens: list[str] = [
     NEW_LINE_TOKEN,
     UNK_TOKEN,
 ]
+
+##dataset
+cc_mined_dir = Path(
+    # elsewhere
+)
+cache_dir = Path(
+    # elsewhere
+)
+BATCH_SIZE: int = 10000
+CONTENT_COLUMN: str = "raw_content"
 c4_sizes: list[int] = [10**7, 3 * 10**6, 3 * 10**6]
 langs: list[str] = ["en", "de", "fr"]
 
+## trainer
 MAX_TOKEN_LENGTH: int = 2**4
 MIN_FREQUENCY: int = 2
 VOCAB_SIZE: int = 2**16
-BATCH_SIZE: int = 10000
+
+## train_tokenizer
+save_tokenizer_dir = Path(
+    # elsewhere
+)
