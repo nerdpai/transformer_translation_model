c8e340a76f818ab0ab55f31befdd0b9f8819b733
nerdpai <mien848red@gmail.com>
2024-05-05 13:51:30 +0200

    spaces separated

--- 2_preprocess_pretrain_dataset/changeable.py
+++ 2_preprocess_pretrain_dataset/changeable.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+
 ## gz
 gz_file_pathes: list[Path] = [
     # elsewhere
 ]
+
 
 ## json
 json_file_pathes: list[Path] = [
@@ -12,4 +14,5 @@ json_file_pathes: list[Path] = [
 csv_directory: Path = Path(
     # elsewhere
 )
+
 TARGET_SIZE_OF_CSV_IN_MB: int = 30
--- 3_create_vocab/changeable.py
+++ 3_create_vocab/changeable.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+
 ## tokenizer
 NEW_LINE_TOKEN: str = "<newline>"
 UNK_TOKEN: str = "<unk>"
 BOS_TOKEN: str = "<s>"
 EOS_TOKEN: str = "</s>"
+
 special_tokens: list[str] = [
     BOS_TOKEN,
     "<pad>",
@@ -14,6 +16,7 @@ special_tokens: list[str] = [
     UNK_TOKEN,
 ]
 
+
 ##dataset
 cc_mined_dir = Path(
     # elsewhere
@@ -21,15 +24,19 @@ cc_mined_dir = Path(
 cache_dir = Path(
     # elsewhere
 )
+
 BATCH_SIZE: int = 10000
 CONTENT_COLUMN: str = "raw_content"
+
 c4_sizes: list[int] = [10**7, 3 * 10**6, 3 * 10**6]
 langs: list[str] = ["en", "de", "fr"]
+
 
 ## trainer
 MAX_TOKEN_LENGTH: int = 2**4
 MIN_FREQUENCY: int = 2
 VOCAB_SIZE: int = 2**16
+
 
 ## train_tokenizer
 save_tokenizer_dir = Path(
