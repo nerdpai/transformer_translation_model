a1a2b1870798487006b7fdb1c646c05685f9769d
nerdpai <mien848red@gmail.com>
2024-05-09 00:29:37 +0200

    remove experimental feature

--- 4_create_embedding/preparations/set_random.py
+++ 4_create_embedding/preparations/set_random.py
@@ -3,7 +3,6 @@ import random
 import numpy as np
 import tensorflow as tf
 from keras import utils
-from tensorflow.config import experimental as tf_exp  # type: ignore
 
 
 def execute(seed: int) -> None:
@@ -13,4 +12,3 @@ def execute(seed: int) -> None:
     tf.random.set_seed(seed)
     tf.keras.utils.set_random_seed(seed)
     utils.set_random_seed(seed)
-    tf_exp.enable_op_determinism()
