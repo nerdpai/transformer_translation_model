f916b0ee0e048e1fc1e00813fe74895e80391872
nerdpai <mien848red@gmail.com>
2024-05-29 20:02:15 +0200

    check integrity before usage

--- 4.5_select_embedding/preparations/model_compile_components/range_decay.py
+++ 4.5_select_embedding/preparations/model_compile_components/range_decay.py
@@ -7,10 +7,14 @@ class RangedDecay(LearningRateSchedule):
         super().__init__()
         self.initial_lr: float = initial_lr
         self.final_lr: float = final_lr
+        self.decay = None  # type: ignore
 
     def set_steps_num(self, steps_num: int) -> None:
         self.decay: float = (self.initial_lr - self.final_lr) / steps_num
 
     def __call__(self, step):
+        if self.decay is None:
+            raise ValueError("steps_num is not set")
+
         lr = self.initial_lr - self.decay * tf.cast(step, tf.float32)
         return tf.maximum(lr, tf.constant(self.final_lr, dtype=tf.float32))
