2ef6c0a783b97476dd63df45d2043da4e55df3af
nerdpai <mien848red@gmail.com>
2024-05-29 20:01:21 +0200

    load without compiling

--- 4.5_select_embedding/preparations/embedding.py
+++ 4.5_select_embedding/preparations/embedding.py
@@ -5,5 +5,5 @@ from keras.models import load_model
 
 
 def execute(emb_path: Path) -> Embedding:
-    model: Sequential = load_model(emb_path)  # type: ignore
+    model: Sequential = load_model(emb_path, compile=False)  # type: ignore
     return model.layers[0]
